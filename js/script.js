b3386b9ba6b4c0f90e370d7957f0e9c0cade395f
Kabilla <a.hadooke3141@gmail.com>
2022-06-21 10:23:15 +0300

    created basic html skeleton, layout and operate function

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+const operate = function (operator, a, b) {
+  if (operator === add) {
+    return add(a, b);
+  } else if (operator === subtract) {
+    return ubtract(a, b);
+  } else if (operator === multiply) {
+    return multiply(a, b);
+  } else if (operator === divide) {
+    return divide(a, b);
+  }
+};
